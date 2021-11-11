1beea0cca4f9576d88cf91748aef7c6bbf635830
Wesley Medeiros da Cruz <wesleymcruz03@gmail.com>
2021-11-10 22:38:50 -0300

    Modificação do arquivo GameScreen.fxml e criação do seu controlador ameScreenController.java

---
+++ src/gui/GameScreenController.java
@@ -0,0 +1,65 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+import util.Utils;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameScreenController implements Initializable {
+
+    @FXML
+    private void onBtn1Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void onBtn2Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void onBtn3Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void onBtn4Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void onBtn5Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void onBtn6Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void onBtn7Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void onBtn8Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void onBtn9Click(ActionEvent event) {
+
+    }
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
